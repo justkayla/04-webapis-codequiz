c72a0d05a35694057c778dad65d28a9cddbb3505
Kayla Justice <kaylarjustice@gmail.com>
2022-09-19 14:46:41 -0500

    JS pseudocode and HTML starter code added.

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+
+// When the page loads, the User is presented with the quiz title, a description, and the start button
+// User clicks on the start button
+// When the start button is clicked, a timer is started and the User is presented with a question
+// If the User answers the question correctly, they are presented with another question
+// If the User answers the question incorrectly, time is subtracted from the timer
+// When all questions are answered OR the timer === 0, then the game is over
+// When the game is over, the User can save their initials and score
+
+
+/* Code for quiz timer
+
+If User answers question correctly, no time is subtracted
+Else User answers question incorrectly, 5 seconds are subtracted from timer
+Total amount of time alloted for quiz is 3 minutes
+
+*/
+
+
+// Code for the EventListener
+
+
+// Code for User name and score keeper
