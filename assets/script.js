e21f1f512354aa7ffb634354c7b5e0512c5839b5
Kayla Justice <kaylarjustice@gmail.com>
2022-09-19 15:25:18 -0500

    Test addEventListeners for start and option buttons.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,35 @@
 
+var startButton = document.querySelector("#start");      // Start button
+var option1 = document.querySelector("#option1");       // First button
+var option2 = document.querySelector("#option2");       // Second button
+var option3 = document.querySelector("#option3");       // Third button
+var option4 = document.querySelector("#option4");       // Fourth button
+
+
+// Listen for click events when unique buttons are pressed
+startButton.addEventListener("click", function() {
+    console.log("start quiz");
+})
+
+option1.addEventListener("click", function() {
+    console.log("option1");
+})
+
+option2.addEventListener("click", function() {
+    console.log("option2");
+})
+
+option3.addEventListener("click", function() {
+    console.log("option3");
+})
+
+option4.addEventListener("click", function() {
+    console.log("option4");
+})
+    // If option1 button is pressed AND the answer === true, move forward   
+    // Else option1 button is pressed AND the answer === false, move forward AND -5 seconds   
+
+
 // When the page loads, the User is presented with the quiz title, a description, and the start button
 // User clicks on the start button
 // When the start button is clicked, a timer is started and the User is presented with a question
