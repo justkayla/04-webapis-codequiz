a1c11c78db882f355129a4d324c62070287d2aca
Kayla Justice <kaylarjustice@gmail.com>
2022-09-21 09:59:44 -0500

    JS showAQuestion function work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,98 @@
 
 
+
+
+
+var currentScore = 0;
+var secondsRemaining = 120;
+// var for current index value as we move through the questions? 
+
+
+
+// Function for showing a question
+function showAQuestion(q){
+  var section = document.createElement("section");
+  var h2 = document.getElementById("question");
+  h2.textContent = q.question;
+    console.log(h2);
+  
+  var ul = document.createElement("ul");
+    console.log(ul);
+    
+  var li = document.querySelectorAll(".answer");
+  li.forEach(function(element, index) {
+    element.textContent = q.answer[index];
+  });
+  
+
+
+
+
+
+  
+  
+
+}
+// Call the function
+showAQuestion(question);
+  
+  // Figure out which item to get from the array whenever the function is called
+  var currentQuestionObj = questions.find(questions[0]);
+  // var currQuestionObj = questions[i];
+  var section = document.createElement("section");
+    console.log(section);
+  var h2 = document.createElement("h2");
+    console.log(h2);
+  var ul = document.createElement("ul");
+    console.log(ul);
+  var li = document.createElement("li");
+    console.log(li);
+// When the User clicks "Start":
+  // Timer starts
+  // Display first question
+    // showAQuestion()
+
+// When a question is displayed:
+  // User must select answer
+  // User must click "next"
+  // 
+    
+
+
+// Array for all question objects
+var questions = [
+  { // Index 0
+    question: "How is Barry so cute?",
+    answer: [ "A1", "A2", "A3", "A4", ],
+    correctAnswer: 1
+  // How to identify correct answer?
+  },
+
+  { // Index 1
+    question: "Why is Barry so amazing?",
+    answer: [ "A1", "A2", "A3", "A4", ],
+    correctAnswer: 3
+  // How to identify correct answer?
+  },
+
+]
+
+// Loop through the array of question objects
+// for( var i = 0; i < questions.length; i ++){
+//   var currQuestionObj = questions[i]
+//   var section = document.createElement("section");
+//     console.log(section);
+//   var h2 = document.createElement("h2");
+//     console.log(h2);
+//   var ul = document.createElement("ul");
+//     console.log(ul);
+//   var li = document.createElement("li");
+//     console.log(li);
+//   //add everything to the DOM
+// }
+
+
+
 // When the page loads, the User is presented with the quiz title, a description, and the start button
 // User clicks on the start button
 // Listen for click events when unique buttons are pressed
@@ -20,69 +113,12 @@
 
 
 // Code for quiz timer
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
+
 
 // Timer that counts down from 120
-function countdown() {
-  var timeLeft = 120;
-
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();         // questions, but outside of timer (take out)
-    }
-  }, 1000);
-}
 
 // Displays the message one word at a time
-function displayMessage() {
-    var wordCount = 0;
-  
-    // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var msgInterval = setInterval(function () {
-      // If there are no more words left in the message
-      if (words[wordCount] === undefined) {
-        // Use `clearInterval()` to stop the timer
-        clearInterval(msgInterval);
-      } else {
-        // Display one word of the message
-        mainEl.textContent = words[wordCount];
-        wordCount++;
-      }
-    }, 1000);
-  }
-  
-  countdown();
 
-var startButton = document.querySelector("#start");           // Start button
-startButton.addEventListener("click", function() {
-    console.log("start quiz");
-})
-
-function startQuiz() {    
-    document.getElementById("count").style="color:green;";
-    startTimer();
-};
-
-startButton.addEventListener("click", function() {
-    console.log("start quiz");
-});
 
 //function that will display current question and global var the current question [0], [1]
 //function display question runs simultaneously with timer
@@ -96,6 +132,9 @@ Total amount of time alloted for quiz is 3 minutes
 
 
 // Code for the EventListener
+  // Clicking "start" button
+  // When they choose a question
+
 
 
 // Code for User name and score keeper
